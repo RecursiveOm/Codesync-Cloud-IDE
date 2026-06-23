acee4750c14368635a432530b846e6248af0a6e4
RecursiveOm <omkar.zunje123@gmail.com>
2026-06-24 05:16:44 +0530

    Initialize frontend workspace with React and Tailwind

---
+++ frontend/src/app/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css"
+
+function App() {
+  return (
+    <main className="h-screen w-full bg-gray-950 text-white flex  gap-4 p-2">
+      <aside className="h-full w-2/5 bg-amber-50 rounded-lg ">
+        
+      </aside>
+      <section className="w-3/4 bg-neutral-800"></section>
+    </main>
+
+
+  )
+}
+
+export default App
