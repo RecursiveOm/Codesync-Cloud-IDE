0a73328e2199d37f485f79b54226c7dccba31dee
RecursiveOm <omkar.zunje123@gmail.com>
2026-06-24 06:00:57 +0530

    Integrate Monaco editor with realtime sync foundation

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,15 +1,53 @@
 import "./App.css"
+import { Editor } from "@monaco-editor/react"
+import { MonacoBinding } from "y-monaco"
+import { useRef, useMemo } from "react"
+import * as y from "yjs"
+import { SocketIOProvider } from "y-socket.io"
 
 function App() {
+  const editorRef = useRef(null)
+  const ydoc = useMemo(() => new y.Doc(), [])
+  const yText = useMemo(() => ydoc.getText("monaco"), [ydoc])
+
+  const handleMount = (editor) => {
+ 
+    editorRef.current = editor
+
+    const provider = new SocketIOProvider(
+      "http://localhost:3000",
+      "monaco",
+      ydoc,
+      {
+        autoConnect: true,
+      }
+    )
+
+    const monacoBinding = new MonacoBinding(
+      yText,
+      editorRef.current.getModel(),
+      new Set([editorRef.current]),
+      provider.awareness
+    )
+
+  }
+
   return (
-    <main className="h-screen w-full bg-gray-950 text-white flex  gap-4 p-2">
-      <aside className="h-full w-2/5 bg-amber-50 rounded-lg ">
-        
+    <main className="h-screen w-full bg-gray-950 text-white flex gap-4 p-2">
+      <aside className="h-full w-2/5 bg-amber-50 rounded-lg">
+
       </aside>
-      <section className="w-3/4 bg-neutral-800"></section>
+
+      <section className="w-3/4 bg-neutral-800">
+        <Editor
+          height="100%"
+          defaultLanguage="javascript"
+          defaultValue="//some comment"
+          theme="vs-dark"
+          onMount={handleMount}
+        />
+      </section>
     </main>
-
-
   )
 }
 
